cea9f3dcbc321f8722d28ee352563588e0a9e939
philipbouwens <philip.bouwens@gmail.com>
2021-06-14 22:04:35 +0200

    Update index.js

--- PD-05/UserDb/index.js
+++ PD-05/UserDb/index.js
@@ -1,25 +1,9 @@
-// Let's use the contract we deployed on the Rinkeby test network!
-// Contract Address: https://rinkeby.etherscan.io/address/0x99BFa4c14D07993f3D1cB03bd27dF9ac8c2E6B8A
+// https://ropsten.etherscan.io/address/0x0a1D071Be3B5C1EbC79ab4118A298508D5C857A8
 
-const contract_address = '0x99BFa4c14D07993f3D1cB03bd27dF9ac8c2E6B8A';
+const contract_address = '0x0a1D071Be3B5C1EbC79ab4118A298508D5C857A8';
 
 // Compile your contract in remix, then go to the .JSON artifact and ABI will be there.
-const abi = [
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_name",
-				"type": "string"
-			}
-		],
-		"name": "create",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
+const Userabi = [
 	{
 		"anonymous": false,
 		"inputs": [
@@ -40,7 +24,19 @@ const abi = [
 		"type": "event"
 	},
 	{
-		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "createUser",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
 		"inputs": [
 			{
 				"internalType": "uint256",
@@ -48,14 +44,31 @@ const abi = [
 				"type": "uint256"
 			}
 		],
-		"name": "remove",
+		"name": "readUser",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "removeUser",
 		"outputs": [],
-		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"constant": false,
 		"inputs": [
 			{
 				"internalType": "uint256",
@@ -68,35 +81,12 @@ const abi = [
 				"type": "string"
 			}
 		],
-		"name": "update",
+		"name": "updateUser",
 		"outputs": [],
-		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"constant": true,
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_id",
-				"type": "uint256"
-			}
-		],
-		"name": "read",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
 		"inputs": [
 			{
 				"internalType": "uint256",
@@ -112,7 +102,6 @@ const abi = [
 				"type": "string"
 			}
 		],
-		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
 	}
@@ -122,10 +111,7 @@ function log(logstr) {
     document.getElementById("log").innerHTML +=logstr+"\n";
 }
 
-
-
 var contract;
-var acts;
 
 async function asyncloaded() {            
     web3 = new Web3(Web3.givenProvider); //provider from metamask
@@ -133,29 +119,31 @@ async function asyncloaded() {
     log(`Version of web3.js: ${web3.version}`);  
     var result = await web3.eth.requestAccounts().catch(x=>{log(x.message); console.log(x);});
     log(`Value from requestAccounts: ${JSON.stringify(result)}`);
-    acts = await web3.eth.getAccounts().catch(log);
+    var acts = await web3.eth.getAccounts().catch(log);
     log(`Here are the accounts: ${JSON.stringify(acts)}`);
 
     var fromadr = acts[0];
-    // toadr = "0xb7a502eb3F6a2De12519a4ED165b5b92c48e131C"
+    toadr = "0xb7a502eb3F6a2De12519a4ED165b5b92c48e131C"
     account(fromadr);
+    contract = new web3.eth.Contract(Userabi, contract_address);
+	log(contract);
+	log(contract_address);
 
-    contract = new web3.eth.Contract(abi, contract_address);
-    // log(`fromadr ${fromadr} has ${Web3.utils.fromWei( 
-    //     await web3.eth.getBalance(fromadr), 'ether')} ether` );
-    // log(`toadr    ${toadr} has ${Web3.utils.fromWei ( 
-    //     await web3.eth.getBalance(toadr), 'ether')} ether` );
-    // log(`Transfering 0.01 ether`);
-    // obj= await web3.eth.sendTransaction({
-    //     from: fromadr,
-    //     to: toadr,
-    //     value: Web3.utils.toWei('0.01', 'ether')
-    // }).catch(x=>log(x.message));
-    // log(`Stored in block ${obj.blockNumber}`)
-    // log(`fromadr ${fromadr} has ${Web3.utils.fromWei ( 
-    //     await web3.eth.getBalance(fromadr), 'ether')} ether` );
-    // log(`toadr    ${toadr} has ${Web3.utils.fromWei ( 
-    //     await web3.eth.getBalance(toadr), 'ether')} ether` );
+    log(`fromadr ${fromadr} has ${Web3.utils.fromWei( 
+        await web3.eth.getBalance(fromadr), 'ether')} ether` );
+    log(`toadr    ${toadr} has ${Web3.utils.fromWei ( 
+        await web3.eth.getBalance(toadr), 'ether')} ether` );
+    log(`Transfering 0.01 ether`);
+    obj= await web3.eth.sendTransaction({
+        from: fromadr,
+        to: toadr,
+        value: Web3.utils.toWei('0.01', 'ether')
+    }).catch(x=>log(x.message));
+    log(`Stored in block ${obj.blockNumber}`)
+    log(`fromadr ${fromadr} has ${Web3.utils.fromWei ( 
+        await web3.eth.getBalance(fromadr), 'ether')} ether` );
+    log(`toadr    ${toadr} has ${Web3.utils.fromWei ( 
+        await web3.eth.getBalance(toadr), 'ether')} ether` );
 }
 window.addEventListener('DOMContentLoaded', asyncloaded);  
 
@@ -164,12 +152,20 @@ function account(accountstr){
 }
 
 function createUser(){
+    var t = "in userfunctie";
+    log(t);
     var userName = document.getElementById("registerName").value;
-    contract.methods.create(userName).send({from: acts[0]}).then(x => console.log(x));
+    log(userName);
+    contract.methods.createUser(userName).send({from: acts[0]});
+    log("gelukt");
 }
 
-function returnUser(){
+async function readUser(){
+    var t = "in readfunctie";
+    log(t);
     var userId = document.getElementById("returnId").value;
-    var result = await contract.methods.read(userId).send({from: acts[0]}).then(x => console.log(x));
+    log(userId);
+    
+    var result = await contract.methods.readUser(userId).call({from: acts[0]}).then(x => {console.log(x); return x});
     document.getElementById("readUserName").innerText = result;
 }
