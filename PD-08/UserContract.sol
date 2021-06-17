f66454b34c4e0532e1dcaec4265b67059f8698e0
philipbouwens <philip.bouwens@gmail.com>
2021-06-17 23:50:32 +0200

    Add files via upload

---
+++ PD-08/UserContract.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier:	AFL-3.0
+pragma solidity ^0.6.0;
+import "github.com/provable-things/ethereum-api/provableAPI_0.6.sol";
+// import "https://raw.githubusercontent.com/provable-things/ethereum-api/master/provableAPI_0.6.sol"
+
+
+contract UserContract is usingProvable{
+  
+  event NewUser(uint userId, string name);
+
+  //Provable variables
+  string  public temp;
+  uint256 public priceOfUrl;
+  constructor() public payable {}
+  
+  struct User{
+    string name;
+  }
+  User[] public users;
+  mapping(string => bool) _userExists;
+  
+  // to create a new user
+  function createUser(string memory _name) public{
+    // requires unique name in order to execute the following code
+    require(!_userExists[_name]);
+    // user will be added to the users array
+    users.push(User(_name));
+    // user.push(); doesnot return int anymore
+    uint id = users.length - 1;
+    emit NewUser(id, _name);
+    // gives value true to name in the mapping
+    _userExists[_name] = true;
+  }
+  
+  // to read the information of the user by giving the id
+  function readUser(uint _id) view public returns(string memory){
+    return users[_id].name;    
+  }
+  
+  // to update the name of the user
+  function updateUser(uint _id, string memory _name) public{
+      // requires unique name in order to execute the following code
+    require(!_userExists[_name]);
+    _userExists[users[_id].name] = false;
+    users[_id].name = _name;
+    _userExists[_name] = true;
+  }
+  
+  // to remove the user
+  function removeUser(uint _id) public{
+   _userExists[users[_id].name] = false;
+   delete users[_id];
+  }
+
+  function __callback(bytes32 /* myid prevent warning*/ , string memory result ) override public {
+       if (msg.sender != provable_cbAddress()) revert();
+       temp = result;
+   }
+
+   function GetTemp() public payable {
+       priceOfUrl = provable_getPrice("URL");
+       require (address(this).balance >= priceOfUrl,
+            "please add some ETH to cover for the query fee");
+       provable_query("URL", 
+            "json(http://weerlive.nl/api/json-data-10min.php?key=demo&locatie=Amsterdam).liveweer[0].temp");
+   }
+      
+}
---
+++ PD-08/index.js
@@ -0,0 +1,241 @@
+// https://ropsten.etherscan.io/address/0x13272C3823B0D2463411801433767bB4C133338F
+
+const contract_address = '0x13272C3823B0D2463411801433767bB4C133338F';
+
+// Compile your contract in remix, then go to the .JSON artifact and ABI will be there.
+const Userabi = [
+	{
+		"inputs": [],
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "userId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"name": "NewUser",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "GetTemp",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "string",
+				"name": "result",
+				"type": "string"
+			}
+		],
+		"name": "__callback",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "_myid",
+				"type": "bytes32"
+			},
+			{
+				"internalType": "string",
+				"name": "_result",
+				"type": "string"
+			},
+			{
+				"internalType": "bytes",
+				"name": "_proof",
+				"type": "bytes"
+			}
+		],
+		"name": "__callback",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "createUser",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "priceOfUrl",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "readUser",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "removeUser",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "temp",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"name": "updateUser",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "users",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+function log(logstr) {   
+    document.getElementById("log").innerHTML +=logstr+"\n";
+}
+
+var contract;
+var acts;
+
+async function asyncloaded() {            
+    web3 = new Web3(Web3.givenProvider); //provider from metamask
+
+    var result = await web3.eth.requestAccounts().catch(x=>{log(x.message); console.log(x);});
+    acts = await web3.eth.getAccounts().catch(log);
+
+
+    var fromadr = acts[0];
+    account(fromadr);
+    contract = new web3.eth.Contract(Userabi, contract_address);
+	
+}
+window.addEventListener('DOMContentLoaded', asyncloaded);  
+
+function account(accountstr){ 
+    document.getElementById("account").innerHTML = accountstr;
+}
+
+function createUser(){
+    var userName = document.getElementById("registerName").value;
+
+    contract.methods.createUser(userName).send({from: acts[0]});
+}
+
+async function readUser(){
+    var userId = document.getElementById("returnId").value;
+   
+    var result = await contract.methods.readUser(userId).call({from: acts[0]}).then(x => {console.log(x); return x});
+    document.getElementById("readUserName").innerText = result;
+}
+
+async function getTemp(){
+    var result = await contract.methods.GetTemp().call().then(x => {console.log(x); return x});   
+    document.getElementById('temperature').innerText = result;
+}
+
+async function getTemp2(){
+    var result = await contract.temp().call().then(x => {console.log(x); return x});   
+    document.getElementById('temperature2').innerText = result;
+}
+
+
+async function getTemp3(){
+    var result = await contract.methods.temp().call().then(x => {console.log(x); return x});   
+    document.getElementById('temperature2').innerText = result;
+}
